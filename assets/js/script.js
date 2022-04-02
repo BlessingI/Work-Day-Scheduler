deb29387c5b5b104173ad04d34d01fbc938483cb
Blessing <bigbadumhe@gmail.com>
2022-04-02 00:21:20 -0500

    persist local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-// let count = timeNow;
 var time = new Date();
-// let timeInHour = time.toLocaleString('en-US', { hour: 'numeric', hour12: true })
 
 let timeInHour = time.getHours()
-
 let textArea = document.querySelector('.description')
 
+let firstButton = document.querySelector('.button1')
+let secondButton = document.querySelector('.button2')
+let threeButton = document.querySelector('.button3')
+let fourthButton = document.querySelector('.button4')
+let fifthButton = document.querySelector('.button5')
+let sixthButton = document.querySelector('.button6')
+let seventhButton = document.querySelector('.button7')
+let eightButton = document.querySelector('.button8')
+let ninthButton = document.querySelector('.button9')
+let tenthButton = document.querySelector('.button10')
 
 let eightSectionTime = parseInt(document.querySelector('#firstRow').innerText.trim())
 let nineSectionTime = parseInt(document.querySelector('#secondRow').innerText.trim())
@@ -21,9 +28,6 @@ let twoSectionTime = parseInt(document.querySelector('#twoRow').innerText.trim()
 let threeSectionTime = parseInt(document.querySelector('#threeRow').innerText.trim())
 let fiveSectionTime = parseInt(document.querySelector('#fiveRow').innerText.trim())
 
-
-
-
 let firstRowText = document.querySelector('#firstRowText');
 let secondRowText = document.querySelector('#secondRowText');
 let fourRowText = document.querySelector('#fourthRowText');
@@ -34,13 +38,6 @@ let oneRowText = document.querySelector('#oneRowText')
 let twoRowText = document.querySelector('#twoRowText')
 let threeRowText = document.querySelector('#threeRowText')
 let fiveRowText = document.querySelector('#fiveRowText')
-
-
-console.log(timeInHour)
-console.log(typeof eightSectionTime)
-console.log(typeof fiveSectionTime)
-
-console.log(eightSectionTime > timeInHour)
 
 
 
@@ -66,3 +63,87 @@ compareHardCodedTime(oneSectionTime, oneRowText)
 compareHardCodedTime(twoSectionTime, twoRowText)
 compareHardCodedTime(threeSectionTime, threeRowText)
 compareHardCodedTime(fiveSectionTime, fiveRowText)
+
+
+
+const contentWritten = () => {
+    let typedText = firstRowText.value.trim()
+    localStorage.setItem("textSaved", JSON.stringify(typedText)); 
+}
+
+const contentWritten2 = () => {
+    let typedText2 = secondRowText.value.trim()
+    localStorage.setItem("textSaved2", JSON.stringify(typedText2)); 
+}
+
+const contentWritten3 = () => {
+    let typedText3 = tenRowText.value.trim()
+    localStorage.setItem("textSaved3", JSON.stringify(typedText3)); 
+}
+
+const contentWritten4 = () => {
+    let typedText4 = elevenRowText.value.trim()
+    localStorage.setItem("textSaved4", JSON.stringify(typedText4)); 
+}
+
+const contentWritten5 = () => {
+    let typedText5 = twelveRowText.value.trim()
+    localStorage.setItem("textSaved5", JSON.stringify(typedText5)); 
+}
+
+const contentWritten6 = () => {
+    let typedText6 = oneRowText.value.trim()
+    localStorage.setItem("textSaved6", JSON.stringify(typedText6)); 
+}
+
+const contentWritten7 = () => {
+    let typedText7 = twoRowText.value.trim()
+    localStorage.setItem("textSaved7", JSON.stringify(typedText7)); 
+}
+
+const contentWritten8 = () => {
+    let typedText8 = threeRowText.value.trim()
+    localStorage.setItem("textSaved8", JSON.stringify(typedText8)); 
+}
+
+const contentWritten9 = () => {
+    let typedText9 = fourRowText.value.trim()
+    localStorage.setItem("textSaved9", JSON.stringify(typedText9)); 
+}
+
+const contentWritten10 = () => {
+    let typedText10 = fiveRowText.value.trim()
+    localStorage.setItem("textSaved10", JSON.stringify(typedText10)); 
+}
+
+
+
+firstRowText.textContent = JSON.parse(localStorage.getItem("textSaved"));
+secondRowText.textContent = JSON.parse(localStorage.getItem("textSaved2"));
+tenRowText.textContent = JSON.parse(localStorage.getItem("textSaved3"));
+elevenRowText.textContent = JSON.parse(localStorage.getItem("textSaved4"));
+twelveRowText.textContent = JSON.parse(localStorage.getItem("textSaved5"));
+oneRowText.textContent = JSON.parse(localStorage.getItem("textSaved6"));
+twoRowText.textContent = JSON.parse(localStorage.getItem("textSaved7"));
+threeRowText.textContent = JSON.parse(localStorage.getItem("textSaved8"));
+fourRowText.textContent = JSON.parse(localStorage.getItem("textSaved9"));
+fiveRowText.textContent = JSON.parse(localStorage.getItem("textSaved10"));
+
+
+firstButton.addEventListener('click', contentWritten)
+secondButton.addEventListener('click', contentWritten2)
+threeButton.addEventListener('click', contentWritten3)
+fourthButton.addEventListener('click', contentWritten4)
+fifthButton.addEventListener('click', contentWritten5)
+sixthButton.addEventListener('click', contentWritten6)
+seventhButton.addEventListener('click', contentWritten7)
+eightButton.addEventListener('click', contentWritten8)
+ninthButton.addEventListener('click', contentWritten9)
+tenthButton.addEventListener('click', contentWritten10)
+
+
+
+
+
+
+
